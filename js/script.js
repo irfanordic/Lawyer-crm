096516be213c95449a178580618c4e1ccb7e3e53
irfanordic@gmail.com <irfanordic@gmail.com>
2025-08-14 16:16:50 +0530

    added a intial add client form , and table . assigned values to and finally added a delete function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,59 @@
 console.log("jsfile is connecte dto index html");
+
+let clients = [];
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("clientForm");
+    
+    
+    form.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const rate = document.getElementById("rate").value;
+    const caseType = document.getElementById("caseType").value;
+
+    const newClient = {name, rate, caseType};
+
+    clients.push(newClient);
+
+    renderClients();
+
+    e.target.reset();
+})
+
+function renderClients(){
+  const tbody = document.querySelector("#clientTable tbody");
+    tbody.innerHTML= " ";
+
+
+    //adding datas to the table 
+    clients.forEach((client, index) =>{
+        const row = document.createElement("tr");
+    
+        row.innerHTML= `
+        <td>${client.name}</td>
+        <td>${client.caseType}</td>
+        <td>$${client.rate}</td>
+        <td> <button class="delete-btn" data-index="${index}">Delete</button> </td>
+      `;
+        tbody.appendChild(row);
+
+
+   
+      })
+      
+      //adding a delete function 
+      document.querySelectorAll(".delete-btn").forEach(button => {
+        button.addEventListener("click", function () {
+          const idx = parseInt(this.getAttribute("data-index"));
+          clients.splice(idx, 1);
+
+          renderClients(tbody);
+        })
+      })
+}}
+
+
+
+)
